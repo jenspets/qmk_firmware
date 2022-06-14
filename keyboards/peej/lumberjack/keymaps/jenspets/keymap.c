8dc8f0bf22ea929769cd5a0a915abb811e4205db
Jens-Petter Sandvik <jens.petter.sandvik@gmail.com>
2022-06-15 00:01:08 +0200

    Update lumberjack keymap to compile with user files

--- keyboards/peej/lumberjack/keymaps/jenspets/keymap.c
+++ keyboards/peej/lumberjack/keymaps/jenspets/keymap.c
@@ -16,29 +16,9 @@
  */
 
 #include QMK_KEYBOARD_H
+#include <jenspets_keymap.h>
 
-#ifdef RGBLIGHT_ENABLE
-static rgblight_config_t rgb_default;
-#endif
-
-#ifndef SWAP_HANDS_ENABLE
-#ifdef SH_T
-#undef SH_T
-#endif
-#define SH_T(kc) kc
-#endif
-
-enum layers {
-    _COLEMAK = 0,
-    _QWERTY,
-    _LOWER,
-    _RAISE,
-    _PLOVER,
-    _ADJUST,
-    _NAV,
-    _NUM
-};
-
+#define LAYOUT_wrap(...)  LAYOUT_ortho_5x12(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {  
     /* Colemak
@@ -187,60 +167,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			       KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL, KC_PEQL, KC_P1, KC_P2, KC_P3, KC_PMNS,
 			       KC_TRNS, KC_TRNS, KC_LGUI, KC_LALT, KC_NO, KC_SPC, KC_BSPC, TG(_NUM), KC_P0, KC_COMM, KC_PDOT, KC_PPLS)
   };
-
-
-  void keyboard_post_init_user(void){
-#ifdef RGBLIGHT_ENABLE
-    rgb_default.raw = eeconfig_read_rgblight();
-#endif
-  }
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-#ifdef RGBLIGHT_ENABLE
-    if (rgblight_is_enabled()){
-	uint8_t offset;
-	switch (get_highest_layer(state)){
-	case _RAISE:
-	    offset = 32;
-	    break;
-	case _LOWER:
-	    offset = -32;
-	    break;
-	case _ADJUST:
-	    /* No offset for adjust layer, as the hue is difficult to adjust with an offset applied */
-	    offset = 0;
-	    break;
-	case _PLOVER:
-	    offset = 128;
-	    break;
-	case _NAV:
-	    offset = 64;
-	    break;
-	case _NUM:
-	    offset = -64;
-	    break;
-	default:
-	    offset = 0;
-	}
-	rgblight_sethsv_noeeprom((rgb_default.hue + offset) % 255, rgb_default.sat, rgb_default.val);
-    }
-#endif
-    return state;
-}
-
-void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode){
-#ifdef RGBLIGHT_ENABLE
-    case RGB_HUD:
-    case RGB_HUI:
-    case RGB_SAD:
-    case RGB_SAI:
-    case RGB_VAD:
-    case RGB_VAI:
-	rgb_default.raw = eeconfig_read_rgblight();
-	break;
-#endif 
-    default:
-	break;
-    }
-}
