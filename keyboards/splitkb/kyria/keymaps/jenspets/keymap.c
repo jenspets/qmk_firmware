21f73f5b6f51a2e8b1a28b6d2c140ab338db4ac9
Jens-Petter Sandvik <jens.petter.sandvik@gmail.com>
2023-03-09 19:09:58 +0100

    Fix qwerty layer

--- keyboards/splitkb/kyria/keymaps/jenspets/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jenspets/keymap.c
@@ -74,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT_wrap(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, CTL_QUOT,
-     LSFT_T(KC_CAPS) , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     KC_NO  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                KC_MPLY , KC_LGUI, ALT_ENT, KC_SPC , KC_QUOT   ,     KC_DEL    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
+			    KC_TAB          , QWR_R2_L ,                                        QWR_R2_R , RALT(KC_W),
+			    X_THE           , QWR_R3_L ,                                        QWR_R3_R , RALT(KC_L),
+			    LSFT_T(KC_CAPS) , QWR_R4_L, KC_NUBS, KC_MINS,     KC_ENT , KC_NUHS, QWR_R4_R, RALT(KC_Z),
+                                     KC_MPLY , KC_LGUI, QWR_T_L, KC_QUOT ,    KC_DEL , QWR_T_R, KC_APP, KC_APP
     ),
 
      [_LOWER] = LAYOUT_wrap(
